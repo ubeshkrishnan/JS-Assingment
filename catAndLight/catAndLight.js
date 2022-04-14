0ac616bff57004def27f426aca4d1b44c683bae3
ubeshkrishnan <103400634+ubeshkrishnan@users.noreply.github.com>
2022-04-14 16:37:14 +0530

    Cat and Light Code

---
+++ catAndLight/catAndLight.js
@@ -0,0 +1,19 @@
+function switchOff() {
+    document.getElementById("bulbImage").src =
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src =
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switched Off";
+    document.getElementById("onSwitch").style.backgroundColor = "#22c55e";
+    document.getElementById("offSwitch").style.backgroundColor = "red";
+  }
+  
+  function switchOn() {
+    document.getElementById("bulbImage").src =
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src =
+      "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "Switched On";
+    document.getElementById("offSwitch").style.backgroundColor = "red";
+    document.getElementById("onSwitch").style.backgroundColor = "green";
+  }
